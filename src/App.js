fb5bae56fcf5468e1fbb115b2e8f81410ea46eb9
Dorina Kamp <dorina.kamp@gmail.com>
2022-11-15 16:59:21 +0100

    changed format

--- src/App.js
+++ src/App.js
@@ -3,66 +3,63 @@ import { useState } from "react";
 import axios from "axios";
 
 const App = () => {
-
   const cityList = ["Amsterdam", "Berlin", "Madrid", "London", "Brussel"];
 
   const [city, setCity] = useState();
 
-
   const mappedButtons = cityList.map((citylisted) => {
     return (
       <div>
-      <button
-        onClick={() => {
-          return setCity(<CityApi key={citylisted} city={citylisted}></CityApi>);
-        }}
-      >
-        {citylisted}
-      </button>
-          </div>
+        <button
+          onClick={() => {
+            return setCity(
+              <CityApi key={citylisted} city={citylisted}></CityApi>
+            );
+          }}
+        >
+          {citylisted}
+        </button>
+      </div>
     );
   });
   console.log(mappedButtons);
   return (
-  <div>
-  <div>{mappedButtons}</div>
-  <div>{city}</div>
-  </div>)
+    <div>
+      <div>{mappedButtons}</div>
+      <div>{city}</div>
+    </div>
+  );
 };
 
 const CityApi = (props) => {
-
   const [selectedCity, setSelectedCity] = useState();
 
   if (!selectedCity) {
     axios
-    .get("https://photon.komoot.io/api/?q=" + props.city)
-    .then((response) => {
-      const { data } = response
+      .get("https://photon.komoot.io/api/?q=" + props.city)
+      .then((response) => {
+        const { data } = response;
 
-      const photonAPI = data.features.map((feature) => {
-        return (
+        const photonAPI = data.features.map((feature) => {
+          return (
+            <div>
+              <div>{feature.properties.country}</div>
+            </div>
+          );
+        });
+        console.log(response);
+        setSelectedCity(
           <div>
-            <div>{feature.properties.country}</div>
+            <div>{photonAPI}</div>
           </div>
-        )
+        );
       })
-      console.log(response)
-      setSelectedCity(
-        <div>
-          <div>{photonAPI}</div>
-        </div>
-      )
-    })
-    .catch((e) => {
-      console.log("Error: " + e);
-   
-    })
+      .catch((e) => {
+        console.log("Error: " + e);
+      });
+  }
 
-  } 
-  
-  return <div>{selectedCity}</div>
-
+  return <div>{selectedCity}</div>;
 };
 
 export default App;
