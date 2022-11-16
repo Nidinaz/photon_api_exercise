bf501e941cec2b530d5aeafbb4f0624e38dba1bd
Mohammad Al Kalaleeb <mohammadautumn@outlook.com>
2022-11-16 19:45:19 +0100

    added form

--- src/App.js
+++ src/App.js
@@ -3,28 +3,41 @@ import { useState } from "react";
 import axios from "axios";
 
 const App = () => {
-
   const [user, setUser] = useState({
-    text: '',
-  })
+    query: "",
+    osmTag: "",
+    ui: null
+  });
 
-  const onTextChange = (field) => {
+  const onCityChanged = (field) => {
     setUser({
-      text: field.target.value,
-    })
-  }
+      query: field.target.value,
+      osmTag: "",
+      ui: null
+    });
+  };
+
+  const onOsmTagChanged = (field) => {
+    setUser({
+      query: user.query,
+      osmTag: field.target.value,
+      ui: null
+    });
+  };
 
   const cityList = ["Amsterdam", "Berlin", "Madrid", "London", "Brussel"];
 
-  const [city, setCity] = useState();
-
   const mappedButtons = cityList.map((citylisted) => {
     return (
-      <div>
+      <div key={Math.random()}>
         <button
           onClick={() => {
-            return setCity(
-              <CityApi key={citylisted} city={citylisted}></CityApi>
+            setUser(
+              <CityApi
+                key={Math.random()}
+                osmTag={"tourism"}
+                city={citylisted}
+              ></CityApi>
             );
           }}
         >
@@ -34,15 +47,48 @@ const App = () => {
     );
   });
 
-  console.log(mappedButtons);
+  // console.log(mappedButtons);
   return (
     <div>
       <div>{mappedButtons}</div>
-      <div>{city}</div>
-      <div className = 'search-form'>
-        <div>
-          <input type ='text' id='text' name="text" placeholder='capitol' value={user.text} onChange={onTextChange}></input>
-        </div>
+
+      <div className="search-form">
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+
+            console.log("Submitting! " + JSON.stringify(user));
+
+            setUser({
+              query: user.query,
+              osmTag: user.osmTag,
+              ui: <CityApi
+                key={Math.random()}
+                osmTag={user.osmTag}
+                city={user.query}
+              ></CityApi>}
+            );
+            
+          }}
+        >
+          <input
+            type="text"
+            id="text"
+            name="text"
+            placeholder="capitol"
+            value={user.text}
+            onChange={onCityChanged}
+          ></input>
+          <input
+            type="text"
+            id="osm"
+            placeholder="osm"
+            value={user.osmTag}
+            onChange={onOsmTagChanged}
+          ></input>
+          <button>Submit</button>
+        </form>
+        <div>{user.ui}</div>
       </div>
     </div>
   );
@@ -52,19 +98,25 @@ const CityApi = (props) => {
   const [selectedCity, setSelectedCity] = useState();
 
   if (!selectedCity) {
+    let baseApi = "https://photon.komoot.io/api/?q=" + props.city;
+    if (props.osmTag) {
+      baseApi = baseApi + "&osm_tag=" + props.osmTag;
+    }
+
+    console.log("Base API: " + baseApi);
     axios
-      .get("https://photon.komoot.io/api/?q=" + props.city)
+      .get(baseApi)
       .then((response) => {
         const { data } = response;
 
         const photonAPI = data.features.map((feature) => {
           return (
-            <div>
-              <div>{feature.properties.country}</div>
+            <div key={Math.random()}>
+              <div>{feature.properties.name}</div>
             </div>
           );
         });
-        console.log(response);
+        // console.log(response);
         setSelectedCity(
           <div>
             <div>{photonAPI}</div>
